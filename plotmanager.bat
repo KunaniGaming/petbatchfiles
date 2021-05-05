3e794ed204f52b8e6d2552bf6895a190beb8b854
Kunani <darin@kunani.com>
2021-05-04 19:37:08 -0700

    Add files via upload

---
+++ plotmanager.bat
@@ -0,0 +1,31 @@
+@echo off
+set /a maxplots = 8
+set /a phase1timesecs = 4400
+set /a initialdelay = 100
+
+for /f "delims=" %%a in (' "wmic process where name="chia.exe" | find "chia.exe" /c" ') do @set currentplots=%%a
+echo ===== Current Plotting Processes: %currentplots% =====
+
+timeout /t %initialdelay% 
+
+:checkplots
+cls
+for /f "delims=" %%a in (' "wmic process where name="chia.exe" | find "chia.exe" /c" ') do @set currentplots=%%a
+echo ===== Current Plotting Processes: %currentplots% =====
+
+if %currentplots% lss %maxplots% (
+ start /min plotk32.bat
+ timeout /t 5
+
+ for /f "delims=" %%a in (' "wmic process where name="chia.exe" | find "chia.exe" /c" ') do @set currentplots=%%a
+ echo ===== Started parallel plot: %currentplots% =====
+ timeout /t %phase1timesecs% 
+
+ goto :checkplots
+) else (
+ cls
+ echo ===== Maximum %currentplots% Plots Processing =====
+ timeout /t 60 
+ goto :checkplots
+)
+
