f94215f650eab3c0c6208404af236499bf38253a
Kunani <darin@kunani.com>
2021-05-09 16:38:20 -0700

    Update createplot.bat

--- createplot.bat
+++ createplot.bat
@@ -1,4 +1,4 @@
 @echo off
-cd C:\Users\USER-NAME-HERE\AppData\Local\chia-blockchain\app-1.1.3\resources\app.asar.unpacked\daemon\
+cd C:\Users\USER-NAME-HERE\AppData\Local\chia-blockchain\app-1.1.5\resources\app.asar.unpacked\daemon\
 chia plots create -x -k 32 -b 3390 -r 2 -n 2 -t t:\ChiaTemp -d t:\ChiaTemp -f FARMER-KEY-HERE -p POOL-KEY-HERE
 exit
